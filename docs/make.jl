7f24a26630172b8b9133a0af1f7cdf5e737b9f4d
Simon B. Adrian <simon.adrian@ieee.org>
2023-01-21 11:54:12 +0100

    Changed to default Julia layout

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,34 @@
 using Documenter, BEAST
 
-makedocs(
-    clean=false,
-    sitename="BEAST Documentation")
+DocMeta.setdocmeta!(BEAST, :DocTestSetup, :(using BEAST); recursive=true)
 
-deploydocs(
-    # deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo   = "github.com/krcools/BEAST.jl.git",
-    # julia  = "1.0",
+makedocs(;
+    clean=false,
+    modules=[BEAST],
+    authors="Kristof Cools <kristof.cools@ugent.be> and contributors",
+    repo="https://github.com/krcools/BEAST.jl/tree/master",
+    sitename="BEAST.jl Documentation",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true", canonical="https://krcools.github.io/BEAST.jl", assets=String[]
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Tutorial" => "tutorial.md",
+        "Assembly" => "assemble.md",
+        "Quadrature Strategies" => "quadstrat.md",
+        #"Manual" => "manual.md",
+        #"Geometry" => Any["Coordinate System" => "coordinateSys.md", "Sphere Dimensions" => "scatterer.md"],
+        #"Excitations" => Any[
+        #    "Plane Wave" => "planeWave.md",
+        #    "Dipoles" => "dipoles.md",
+        #    "Ring Currents" => "ringCurrents.md",
+        #    "Spherical Modes" => "sphModes.md",
+        #    "Uniform Static Field" => "uniformStatic.md",
+        #],
+        #"Further Details" => "details.md",
+        #"Contributing" => "contributing.md",
+        #"API Reference" => "apiref.md",
+    ],
 )
+
+deploydocs(; repo="github.com/krcools/BEAST.jl.git")
